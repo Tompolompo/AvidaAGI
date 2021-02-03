c35c0c9512d3b0e5cf66465d7a9331baffb0d91e
tompolompo <tomas.lundberg95@gmail.com>
2021-02-03 08:57:42 +0100

    Wrapper update elapsed time and expected remaining runtime

--- wrapper.py
+++ wrapper.py
@@ -1,12 +1,19 @@
 import subprocess
+import time
+import numpy as np
 
 def run_avida(n, m, u, i):
     subprocess.call(["./avida", "-n" + str(n), "-m" + str(m), "-u" + str(u), "-i" + str(i)], )
 
-N=8
-M=2
-U=100
-print("Running with " + str(N) +  " worlds,  " + str(M) + " meta generations and " + str(U) + " updates.")
+N=12
+M=40
+U=2500
+print("Running with " + str(N) +  " worlds, " + str(M) + " meta generations and " + str(U) + " updates.")
+
+times = []
 for i in range(M):
+    start = time.perf_counter()
     run_avida(n=N, m=M, u=U, i=i)
+    times.append(time.perf_counter()-start)
+    print("Tot elapsed: " + str(round(sum(times)/3600, 2)) + " h, remaining: " + str(round(np.mean(times)/3600*(M-i),2)) + " h")
 
