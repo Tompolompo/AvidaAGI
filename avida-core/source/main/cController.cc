9597dd8dc7691682df0daa35f6f2220ed503c6ba
Richard Martin <ricmarti@student.chalmers.se>
2021-02-18 13:59:27 +0100

    Tagit bort hårdkodade chromosomer ur controllerns contrstructor

--- avida-core/source/main/cController.cc
+++ avida-core/source/main/cController.cc
@@ -5,11 +5,11 @@ using namespace std;
 using namespace Controller;
 
 cController::cController() {
-    Phi_0[0]=1;Phi_0[1]=1;Phi_0[2]=2;Phi_0[3]=2;Phi_0[4]=3;
-    Phi_0[5]=3;Phi_0[6]=4;Phi_0[7]=4;Phi_0[8]=5;
-    m_chromosome[0]=1;m_chromosome[1]=1;m_chromosome[2]=2;m_chromosome[3]=2;m_chromosome[4]=3;
-    m_chromosome[5]=3;m_chromosome[6]=4;m_chromosome[7]=4;m_chromosome[8]=5;
-    m_chromosome_length = 9;
+    // Phi_0[0]=1;Phi_0[1]=1;Phi_0[2]=2;Phi_0[3]=2;Phi_0[4]=3;
+    // Phi_0[5]=3;Phi_0[6]=4;Phi_0[7]=4;Phi_0[8]=5;
+    // m_chromosome[0]=1;m_chromosome[1]=1;m_chromosome[2]=2;m_chromosome[3]=2;m_chromosome[4]=3;
+    // m_chromosome[5]=3;m_chromosome[6]=4;m_chromosome[7]=4;m_chromosome[8]=5;
+    // m_chromosome_length = 9;
 }
 
 void cController::SetChromosome(double chromosome[], int len){
@@ -20,11 +20,24 @@ void cController::SetChromosome(double chromosome[], int len){
 }
 
 void cController::SetRefChromosome(double chromosome[], int len){
-    m_chromosome_length = len;
     for (int i=0; i<len; i++){
         Phi_0[i]=chromosome[i];
     }
 }
+
+void cController::PrintChromosome(int which) {
+    for (int i=0; i<m_chromosome_length; i++){
+        if (which == 1) {
+            std::cout << Phi_0[i] << " ";
+        }
+        else    {
+            std::cout << m_chromosome[i] << " ";
+        }
+        
+    }
+    std::cout << std::endl;
+}
+
 
 
 Avida::Genome cController::controll_genome(Avida::Genome* old_genome){
--- avida-core/source/main/cController.h
+++ avida-core/source/main/cController.h
@@ -13,6 +13,7 @@ namespace Controller{
         double m_chromosome[9];
         int m_chromosome_length;
         void SetChromosome(double chromosome[], int len);
+        void PrintChromosome(int which);
         void SetRefChromosome(double chromosome[], int len);
         Avida::Genome controll_genome(Avida::Genome* genome);
 
